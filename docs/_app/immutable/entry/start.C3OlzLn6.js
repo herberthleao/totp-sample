fb0a47974407c45b4952ff1a92bc307fa406f0cd
Herberth Leão <dev@herberthleao.com>
2024-09-25 19:22:13 -0300

    chore: add github workflow

--- docs/_app/immutable/entry/start.C3OlzLn6.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DWnBswjm.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,14 +12,10 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      pages: 'docs',
-      assets: 'docs',
       fallback: undefined,
-      precompress: false,
-      strict: true,
     }),
     paths: {
-        base: '/totp-sample'
+        base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     }
   },
 };
